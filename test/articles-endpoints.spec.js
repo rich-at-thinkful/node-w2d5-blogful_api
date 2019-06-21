b245f7e9e0a8457c91697276abf084738200b295
MrSkinny <rgnet1@yahoo.com>
2019-06-20 17:38:29 -0700

    fix setup

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
 const app = require('../src/app')
 const { makeArticlesArray, makeMaliciousArticle } = require('./articles.fixtures')
+const config = require('../src/config');
 
 describe('Articles Endpoints', function() {
   let db
@@ -8,7 +9,7 @@ describe('Articles Endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: config.TEST_DATABASE_URL,
     })
     app.set('db', db)
   })
